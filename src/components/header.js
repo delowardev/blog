8f41823c7daabf4af604960d9287374d6b2d34e0
Delowar Hossain <delowardev@gmail.com>
2021-04-20 10:50:12 +0600

    header adjustment

--- src/components/header.js
+++ src/components/header.js
@@ -22,26 +22,28 @@ const Header = ({ siteTitle }) => {
     return (
         <header className="header">
             <div className="container">
-                <div className="row justify-content-between align-items-center">
-                    <div className="col-auto">
-                        <Link className='branding' to='/'>
-                            {siteTitle}
-                        </Link>
-                    </div>
-                    <div className="col-auto">
-                        <nav>
-                            {
-                                nav.map((n, k) => (
-                                    <Link
-                                        key={k}
-                                        activeClassName="is-active"
-                                        to={n.to}
-                                    >
-                                        {n.name}
-                                    </Link>
-                                ))
-                            }
-                        </nav>
+                <div className="header-inner">
+                    <div className="row justify-content-between align-items-center">
+                        <div className="col-auto">
+                            <Link className='branding' to='/'>
+                                {siteTitle}
+                            </Link>
+                        </div>
+                        <div className="col-auto">
+                            <nav>
+                                {
+                                    nav.map((n, k) => (
+                                        <Link
+                                            key={k}
+                                            activeClassName="is-active"
+                                            to={n.to}
+                                        >
+                                            {n.name}
+                                        </Link>
+                                    ))
+                                }
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </div>
