e37bf56f2eb5979789f5dfab05f3a5266ac99d2f
Delowar Hossain <delowardev@gmail.com>
2021-04-20 15:26:34 +0600

    form finished

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useForm, ValidationError } from "@formspree/react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {useState} from "react";
 
 
 const toastSetting = {
@@ -17,6 +18,14 @@ const toastSetting = {
 const Contact = () => {
 
     const [state, handleSubmit] = useForm(process.env.GATSBY_FORMSPREE);
+    const [name, setName] = useState("");
+    const [company, setCompany] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+
 
     const showError = (msg = null) => {
         toast.error(msg ? msg : 'Something is wrong! Please try again later.', toastSetting)
@@ -24,6 +33,12 @@ const Contact = () => {
 
     const showSuccess = () => {
         toast.success('Thanks for contacting, I will get back to you soon.', toastSetting)
+        setName("");
+        setCompany("");
+        setEmail("")
+        setPhone("")
+        setSubject("")
+        setMessage("")
     }
 
     const submit = (e) => {
@@ -56,7 +71,17 @@ const Contact = () => {
                                 <div className="row form-row">
                                     <div className="col-6">
                                         <label htmlFor="name">Name *</label>
-                                        <input required type="text" name="name"/>
+                                        <input
+                                            value={name}
+                                            required
+                                            type="text"
+                                            name="name"
+                                            onChange={
+                                                (e) => {
+                                                    setName(e.target.value);
+                                                }
+                                            }
+                                        />
                                         <ValidationError
                                             prefix="Name"
                                             field="name"
@@ -65,7 +90,16 @@ const Contact = () => {
                                     </div>
                                     <div className="col-6">
                                         <label htmlFor="company">Company</label>
-                                        <input type="text" name="company"/>
+                                        <input
+                                            value={company}
+                                            type="text"
+                                            name="company"
+                                            onChange={
+                                                (e) => {
+                                                    setCompany(e.target.value);
+                                                }
+                                            }
+                                        />
                                         <ValidationError
                                             prefix="Company"
                                             field="company"
@@ -76,7 +110,18 @@ const Contact = () => {
                                 <div className="row form-row">
                                     <div className="col-6">
                                         <label htmlFor="email">Email *</label>
-                                        <input required placeholder="email@example.com" type="email" name="email"/>
+                                        <input
+                                            value={email}
+                                            required
+                                            placeholder="email@example.com"
+                                            type="email"
+                                            name="email"
+                                            onChange={
+                                                (e) => {
+                                                    setEmail(e.target.value);
+                                                }
+                                            }
+                                        />
                                         <ValidationError
                                             prefix="Email"
                                             field="email"
@@ -85,7 +130,17 @@ const Contact = () => {
                                     </div>
                                     <div className="col-6">
                                         <label htmlFor="phone">Phone Number</label>
-                                        <input placeholder="(xxx) xxx-xxx" type="tel" name="phone"/>
+                                        <input
+                                            value={phone}
+                                            placeholder="(xxx) xxx-xxx"
+                                            type="tel"
+                                            name="phone"
+                                            onChange={
+                                                (e) => {
+                                                    setPhone(e.target.value);
+                                                }
+                                            }
+                                        />
                                         <ValidationError
                                             prefix="Phone"
                                             field="phone"
@@ -96,7 +151,17 @@ const Contact = () => {
                                 <div className="row form-row">
                                     <div className="col-12">
                                         <label htmlFor="subject">Subject *</label>
-                                        <input required type="text" name="subject"/>
+                                        <input
+                                            value={subject}
+                                            required
+                                            type="text"
+                                            name="subject"
+                                            onChange={
+                                                (e) => {
+                                                    setSubject(e.target.value);
+                                                }
+                                            }
+                                        />
                                         <ValidationError
                                             prefix="Subject"
                                             field="subject"
@@ -107,7 +172,16 @@ const Contact = () => {
                                 <div className="row">
                                     <div className="col-12">
                                         <label htmlFor="message">Your message *</label>
-                                        <textarea required name="message" />
+                                        <textarea
+                                            value={message}
+                                            required
+                                            name="message"
+                                            onChange={
+                                                (e) => {
+                                                    setMessage(e.target.value);
+                                                }
+                                            }
+                                        />
                                         <ValidationError
                                             prefix="Message"
                                             field="message"
