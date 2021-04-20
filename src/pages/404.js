ca62f8dd17473f50973d30c0f3d6db2a95342922
Delowar Hossain <delowardev@gmail.com>
2021-04-21 01:41:37 +0600

    404 updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,20 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {Link} from "gatsby";
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container">
+        <SEO title="404: Not found" />
+        <div className="error-404">
+            <div className="spacing is-lg" />
+            <h1>Sorry, this page isn't available.</h1>
+            <p>You may have mistyped the address or the page may have moved.</p>
+            <Link className="btn is-primary" to={'/'}>Go to home</Link>
+            <div className="spacing is-lg" />
+        </div>
+    </div>
   </Layout>
 )
 
